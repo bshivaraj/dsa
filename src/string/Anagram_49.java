6630e8f0b71b0e4a5917ff0a5795ade7fb27706c
shivarajbiradar <shivarajbiradar66@gmail.com>
2025-02-02 13:14:04 +0530

    group of Anagram_49

---
+++ src/string/Anagram_49.java
@@ -0,0 +1,27 @@
+package string;
+
+import java.util.*;
+
+public class Anagram_49 {
+    public static void main(String[] args) {
+        String[] strs= {"eat","tea","tan","ate","nat","bat"};
+        List<List<String>> output=getAnagram(strs);
+        System.out.println(output);
+    }
+
+    private static List<List<String>> getAnagram(String[] strs) {
+       List<List<String>> res=new ArrayList<>();
+       Map<String,List<String>> map=new HashMap<>();
+
+       for(int i=0;i<strs.length;i++) {
+           char[] c = strs[i].toCharArray();
+           Arrays.sort(c);
+           String key = String.valueOf(c);
+           map.computeIfAbsent(key,k->new ArrayList<>()).add(strs[i]);
+       }
+
+       map.keySet().forEach(x->res.add(map.get(x)));
+       return res;
+    }
+
+}
