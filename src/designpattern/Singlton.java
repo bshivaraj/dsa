6d739bd7074be0e6dd63c052a1d45cc9ccc10dca
shivarajbiradar <shivarajbiradar66@gmail.com>
2025-02-11 09:45:10 +0530

    new problems

---
+++ src/designpattern/Singlton.java
@@ -0,0 +1,30 @@
+package designpattern;
+
+public class Singlton {
+    public static void main(String[] args) {
+
+         Singlton obj = null;
+         int count=0;
+         Counter counter=new Counter();
+
+         Singlton s1=getInstance(obj,count);
+         Singlton s2=getInstance(obj,count);
+         Singlton s3=getInstance(obj,count);
+        Singlton s4=getInstance(obj,count);
+
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+    private static Singlton getInstance(Singlton obj,int count){
+        if(obj==null && count<3){
+            obj=new Singlton();
+            count++;
+        }
+        return obj;
+    }
+}
+class Counter{
+
+}
